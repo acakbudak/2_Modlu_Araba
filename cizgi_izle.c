9a0bb1fb13036d92469dc06885857bb6a5ce782b
Ahmet AKBUDAK <47061565+acakbudak@users.noreply.github.com>
2020-01-07 17:46:10 +0300

    Create cizgi_izle.c

---
+++ cizgi_izle.c
@@ -0,0 +1,25 @@
+/*
+ * Cizgi izleyen robot modu icin gerekli arduino loop kodu
+ */
+
+void park_et(void)
+{
+  int sensor_1= analogRead(A6);
+  int sensor_2= analogRead(A7);
+
+  if( sensor_1<400 && sensor_2<400)
+  {
+    ileri_git_yavas();
+
+  }
+  if( sensor_1>400 && sensor_2<400)
+  {
+    sola_don();
+  }
+  if( sensor_1<400 && sensor_2>400)
+  {
+    saga_don();
+  }
+
+  
+}
